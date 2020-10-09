0ea1efcafad6940fc1458c5b684ef3b6823898fe
mpmoran <kamokumoran@gmail.com>
2020-10-09 10:39:22 -0700

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
 class User < ActiveRecord::Base
+    validates :name, presence: true
+    has_secure_password
+    has_many :rides
+    has_many :attractions, through: :rides
+
+    def mood
+        if self.admin == false
+            if self.nausea > self.happiness
+                'sad'
+            else
+                'happy'
+            end
+        else
+            "Not applicable"
+        end
+    end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:new, :show, :create]
+  resources :attractions
+  resources :rides, only: [:create]
+  resources :sessions, only: [:create]  
+
+  get '/signin', to: 'sessions#new'
+  get '/logout', to: 'sessions#destroy'  
+  get '/signup', to: 'users#new'
+
+  root 'static#home' 
+  #get '/users', to:'users#index', as: 'users'
+  #post '/users', to:'users#create'
+  #get '/users/:id', to: 'users#show', as: 'user'
+  #get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
+  #get '/attractions', to: 'attractions#index', as: 'attractions'
+  #get '/signin', to: 'session#new', as: 'signin'
+  #post '/session', to: 'session#create', as: 'session'
+  #delete '/session/', to: 'session#destroy'
+  #get '/attractions/new', to: 'attractions#new', as: 'new_attraction'
+  #get '/attractions/:id', to: 'attractions#show', as: 'attraction'
+  #get '/attractions/:id/edit', to: 'attractions#edit', as: 'edit_attraction'
+  #patch '/attractions/:id', to: 'attractions#update'
+  #post '/attractions', to: 'attractions#create'
+  #post '/rides', to:"rides#create", as: 'rides'
 end
